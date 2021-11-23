4ea8a529b8907b3ee91a7e1742aa6b2570aca792
Marco Cerri <marcorenatocerri@gmail.com>
2021-11-23 16:13:13 +0100

    feat: seeds, index and show. fix: dogs-controller"

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Dog.destroy_all
+User.destroy_all
+puts "All registered users have been destroyed"
+
+user2 = User.create(nickname: "user2", email: "user@email.com", password: "123123")
+User.all
+puts "#{user2} has been created"
+
+Dog.create!(name: 'Lola', breed: 'husky', gender: 'female', pedigree: true, address: 'Via Col Di lana 6, Milan',
+            user: user2, matchable: true, birthdate: '14/04/17')
