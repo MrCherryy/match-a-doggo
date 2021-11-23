80a755a75339c00012bc7995cb9651894a9a4af0
Salvatore Strangio <sal.strangio@gmail.com>
2021-11-23 14:49:22 +0100

    Fix: Dog model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_171404) do
+ActiveRecord::Schema.define(version: 2021_11_23_134612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2021_11_22_171404) do
   create_table "dogs", force: :cascade do |t|
     t.string "name"
     t.string "breed"
-    t.integer "age"
     t.string "gender"
     t.boolean "pedigree"
     t.string "address"
@@ -26,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_11_22_171404) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "birthdate"
     t.index ["user_id"], name: "index_dogs_on_user_id"
   end
 
