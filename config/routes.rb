aab87c719aa9cbcad783900d95258bbac24f6770
Salvatore Strangio <sal.strangio@gmail.com>
2021-11-23 13:13:37 +0100

    Feat: Validations, Dogs Controlle. Fix: Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'dogs/index'
+  get 'dogs/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :dogs, only: %i[index show]
+  resources :dogs
 end
