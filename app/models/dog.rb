7eb47f5dd9dd4916977f03b18cf1538edff9d3b7
Marco Cerri <marcorenatocerri@gmail.com>
2021-11-24 15:00:21 +0100

    Feat: matches controller. Fix: routes, dog model

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,9 +4,19 @@ class Dog < ApplicationRecord
 
   validates :name, :breed, :birthdate, :gender, :pedigree, :address, presence: true
 
+  GENDER = %w[male female]
+
+  # simple form per scegliere il gender
+  # <%= simple_form_for do |f| %>
+  # <%= f.select Dog::GENDER %>
+
   def age
     year = ((Time.zone.now - birthdate.to_time) / 1.year.seconds).floor
     month = ((Time.zone.now - year - birthdate.to_time) / 1.month.seconds).floor
     { year: year, month: month }
+  end
+
+  def self.of_gender(gender)
+    Dog.where(gender: gender)
   end
 end
