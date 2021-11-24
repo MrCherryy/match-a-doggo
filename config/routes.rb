04a2e106553556f0dffc4d5005d5240d1ac8fca0
Alessia Garbi <lalebi@live.com>
2021-11-24 17:33:59 +0100

    fixed prifile and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   get 'matches/new'
   get 'matches/index'
+
   devise_for :users
   root to: 'pages#home'
+
   resources :dogs do
     resources :matches, only: :create
   end
+
   resources :matches, only: :index
 
   # routes for styleguides DO NOT TOUCH
