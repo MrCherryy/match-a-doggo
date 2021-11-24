df2318bae2580c6b95c9d063a5ee91dc7cf75223
Marco Cerri <marcorenatocerri@gmail.com>
2021-11-24 18:22:07 +0100

    fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
-  get 'matches/new'
-  get 'matches/index'
-
   devise_for :users
   root to: 'pages#home'
 
   resources :dogs do
-    resources :matches, only: :create
+    resources :matches, only: [:index, :create]
   end
 
-  resources :matches, only: :index
+  # resources :matches, only: :index
 
   # routes for styleguides DO NOT TOUCH
   get "/stylesheets/dogs/index", to: "stylesheets#index", as: :index
