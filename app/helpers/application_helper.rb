d4db7b12f81a3f24c3be18de97c013974a51fa8e
hadi khansa <hadi.khansa@lau.edu>
2021-11-30 11:48:57 +0100

    Fix: navbar done with 4 tabs instead of 3 and style has been implemented

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def current_dog
     session[:dog_id] ? Dog.find(session[:dog_id]) : Dog.new
   end
+
+  def active_class(path)
+    if request.path == path
+      return 'active-icon'
+    else
+      return ''
+    end
+  end
 end
