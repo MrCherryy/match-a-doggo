048329ad1ece2c5a1bcc8d0abe6d9c48798abf4c
Salvatore Strangio <sal.strangio@gmail.com>
2021-11-30 15:22:19 +0100

    Fix: width in Form New

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -3,10 +3,11 @@ import 'select2';
 
 const initSelect2 = () => {
   $('#dog_breed').select2({
-    placeholder: 'Select a breed'
+    placeholder: 'Select a breed',
+    width: '66%'
   });
   $('#dog_gender').select2({
-    width: '30%'
+    width: '32%'
   }
   );
 };
