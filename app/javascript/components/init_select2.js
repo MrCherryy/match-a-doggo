5ea2a2cfd1960b8ff6c22bf4ab2f73b1a7d5cfcb
Salvatore Strangio <sal.strangio@gmail.com>
2021-11-30 17:32:43 +0100

    Fix: Resized breed input on New Form

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -3,7 +3,8 @@ import 'select2';
 
 const initSelect2 = () => {
   $('#dog_breed').select2({
-    placeholder: 'Select a breed'
+    placeholder: 'Select a breed',
+    width: '66%'
   });
   $('#dog_gender').select2({
     width: '30%'
