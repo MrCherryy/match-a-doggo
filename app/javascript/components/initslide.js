39fdc83227c84b9a61a9c7f7433c6255d88a3149
hadi khansa <hadi.khansa@lau.edu>
2021-12-02 11:09:01 +0100

    Fix: the matches page bars are working smoothly now

--- app/javascript/components/initslide.js
+++ app/javascript/components/initslide.js
@@ -12,11 +12,10 @@ const slide = () => {
 
     function initCards(card, index) {
       var newCards = document.querySelectorAll('.tinder--card:not(.removed)');
-
-      newCards.forEach(function (card, index) {
-        card.style.zIndex = allCards.length - index;
-        card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
-        card.style.opacity = (10 - index) / 10;
+        newCards.forEach(function (card, index) {
+          card.style.zIndex = allCards.length - index;
+          card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
+          card.style.opacity = (10 - index) / 10;
       });
 
       tinderContainer.classList.add('loaded');
